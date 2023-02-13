ef0d274dfbaab1c5c136ea8963dbc963a92db68e
Nícolas Galdino <nicolasesmael1998@gmail.com>
2023-02-13 17:04:58 -0300

    First commit

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from recipes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', home, name="home"),
+    path('', home),
 ]

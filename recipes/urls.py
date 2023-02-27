16514692b1fed0f1729c4a3bbd6dccfc75e9c84b
Nícolas Galdino <nicolasesmael1998@gmail.com>
2023-02-27 12:27:23 -0300

    Seção dos Templates finalizada.

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from recipes.views import (
-    home,
-)
+from . import views
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path('recipes/<int:id>/', views.recipe, name='recipe'),
 ]

fe6bbe369221cea35d6ed6b7e6e9c015904ee9e2
Nícolas Galdino <nicolasesmael1998@gmail.com>
2023-02-28 14:04:57 -0300

    Adicionados alguns testes inicias para conferir o funcionamento das views e URL's.

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from recipes import views
+from django.urls import (
+    reverse,
+    resolve,
+)
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_if_recipe_home_url_is_working(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
+
+    def test_if_recipe_category_url_is_working(self):
+        category_url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(category_url, '/recipes/category/1/')
+
+    def test_if_recipe_recipe_detail_url_is_working(self):
+        recipe_detail_url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(recipe_detail_url, '/recipes/1/')
+
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_view_function_is_working(self):
+        home_view = resolve(reverse('recipes:home'))
+        self.assertIs(home_view.func, views.home)
+
+    def test_recipe_category_view_function_is_working(self):
+        category_view = resolve(
+            reverse(
+                'recipes:category',
+                kwargs={'category_id': 1},
+            )
+        )
+        self.assertIs(category_view.func, views.category)
+
+    def test_recipe_recipe_view_function_is_working(self):
+        recipe_view = resolve(
+            reverse(
+                'recipes:recipe',
+                kwargs={'id': 1},
+            )
+        )
+        self.assertIs(recipe_view.func, views.recipe)

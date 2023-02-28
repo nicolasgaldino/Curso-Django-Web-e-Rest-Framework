4a6598a250eaa6791272f42ee7400db9310e0aa0
Nícolas Galdino <nicolasesmael1998@gmail.com>
2023-02-28 10:18:09 -0300

    Nome do projeto alterado para 'project', a fim de manter a uniformidade das normações e arquivos e diretórios..

--- recipes/views.py
+++ recipes/views.py
@@ -2,11 +2,16 @@ from recipes.models import Recipe
 from django.shortcuts import (
     render,
     get_list_or_404,
+    get_object_or_404,
 )
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            is_published=True,
+        ).order_by('id')
+    )
     return render(request, "recipes/pages/home.html", context={
         'recipes': recipes
     })
@@ -26,10 +31,11 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
+    recipe = get_object_or_404(
+        Recipe,
         id=id,
         is_published=True,
-    ).order_by('id').first()
+    )
     return render(request, "recipes/pages/recipe-view.html", context={
         'recipe': recipe,
         'is_detail_page': True,

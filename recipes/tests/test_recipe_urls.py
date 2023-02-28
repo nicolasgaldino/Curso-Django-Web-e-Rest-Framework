48bca69f26ee131634c9a1a6788a257d23f1a9f1
Nícolas Galdino <nicolasesmael1998@gmail.com>
2023-02-28 14:12:50 -0300

    Todos os testes movidos para o package chamado 'tests', com o um arquivos '__init__' dentro dele.

---
+++ recipes/tests/test_recipe_urls.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class RecipeURLsTest(TestCase):
+    def test_if_recipe_home_url_is_working(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
+
+    def test_if_recipe_category_url_is_working(self):
+        category_url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(category_url, '/recipes/category/1/')
+
+    def test_if_recipe_recipe_detail_url_is_working(self):
+        recipe_detail_url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(recipe_detail_url, '/recipes/1/')
--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -6,20 +6,6 @@ from django.urls import (
 )
 
 
-class RecipeURLsTest(TestCase):
-    def test_if_recipe_home_url_is_working(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
-
-    def test_if_recipe_category_url_is_working(self):
-        category_url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(category_url, '/recipes/category/1/')
-
-    def test_if_recipe_recipe_detail_url_is_working(self):
-        recipe_detail_url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(recipe_detail_url, '/recipes/1/')
-
-
 class RecipeViewsTest(TestCase):
     def test_recipe_home_view_function_is_working(self):
         home_view = resolve(reverse('recipes:home'))

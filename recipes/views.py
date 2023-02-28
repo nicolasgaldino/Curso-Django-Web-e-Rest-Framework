db070d36168d5dd3dd7f90afc61c03badb7c3eff
Nícolas Galdino <nicolasesmael1998@gmail.com>
2023-02-28 09:41:29 -0300

    Tratamento de erros adicionado.

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
+from django.shortcuts import (
+    render,
+    get_list_or_404,
+)
 
 
 def home(request):
@@ -11,16 +13,24 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True
-    ).order_by('id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('id')
+    )
     return render(request, "recipes/pages/category.html", context={
-        'recipes': recipes
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | ',
     })
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        id=id,
+        is_published=True,
+    ).order_by('id').first()
     return render(request, "recipes/pages/recipe-view.html", context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })

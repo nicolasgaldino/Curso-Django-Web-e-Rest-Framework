322202c749af3665a3c22571d61e7d01553f743e
Nícolas Galdino <nicolasesmael1998@gmail.com>
2023-03-20 13:02:31 -0300

    Correção do range final da paginação.

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -8,7 +8,7 @@ class PaginationTest(TestCase):
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=1,
-        )
+        )['pagination']
         self.assertEqual([1, 2, 3, 4], pagination)
 
     def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):  # noqa 501
@@ -16,5 +16,21 @@ class PaginationTest(TestCase):
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=3,
-        )
+        )['pagination']
         self.assertEqual([2, 3, 4, 5], pagination)
+
+    def test_make_sure_middle_range_are_correct(self):
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=12,
+        )['pagination']
+        self.assertEqual([11, 12, 13, 14], pagination)
+
+    def test_make_pagination_range_is_static_when_last_page_in_next(self):
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=19,
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)

8cf749160fe26267da66d9ea70805ebcf3f2b418
Nícolas Galdino <nicolasesmael1998@gmail.com>
2023-03-20 13:32:41 -0300

    PAginação adicionada no template.

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,7 @@ from .models import Recipe
 from django.db.models import Q
 from django.http.response import Http404
 from django.core.paginator import Paginator
+from utils.pagination import make_pagination_range
 from django.shortcuts import (
     render,
     get_list_or_404,
@@ -14,12 +15,23 @@ def home(request):
             is_published=True
         ).order_by('-id')
 
-    current_page = request.GET.get('page', 1)
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
     paginator = Paginator(recipes, 9)
     page_object = paginator.get_page(current_page)
 
+    pagination_range = make_pagination_range(
+            paginator.page_range,
+            4,
+            current_page,
+        )
+
     return render(request, "recipes/pages/home.html", context={
             'recipes': page_object,
+            'pagination_range': pagination_range,
         })
 
 

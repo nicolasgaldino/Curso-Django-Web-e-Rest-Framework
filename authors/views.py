d64e7b45835c317a40b92b41af9bf13529351eb3
Nícolas Galdino <nicolasesmael1998@gmail.com>
2023-03-22 13:09:33 -0300

    Iniciando o formulário para registro.

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .forms import RegisterForm
 
 
 def register_view(request):
-    return render(request, 'authors/pages/register_view.html')
+    form = RegisterForm()
+    return render(request, 'authors/pages/register_view.html', {
+        'form': form,
+    })

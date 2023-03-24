e6524b607d3f37cc70b785bae786b878fb5ac370
Nícolas Galdino <nicolasesmael1998@gmail.com>
2023-03-24 09:12:42 -0300

    Deveria ter feito esse push ontem, mas tive problemas com o 'index.lock' do Git. De qualque forma, fiz algumas altereções no 'forms.py', e nos estilos gerias também. Demorei tanto para resolver esse problema que até perdi o fio da miada do que estava fazendo ontem.

--- authors/forms.py
+++ authors/forms.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(forms.ModelForm):
+    password_confirm = forms.CharField(
+        label='Confirmação de senha:',
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repita sua senha',
+            'class': 'placeholder-text',
+        }),
+    )
+
     class Meta:
         model = User
         fields = [
@@ -12,3 +20,32 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        labels = {
+            'first_name': 'Primeiro nome:',
+            'last_name': 'Último nome:',
+            'username': 'Nome de usuário:',
+            'email': 'E-mail:',
+            'password': 'Senha:',
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Ex.: John',
+                'class': 'placeholder-text',
+            }),
+            'last_name': forms.TextInput(attrs={
+                'placeholder': 'Ex.: Doe',
+                'class': 'placeholder-text',
+            }),
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Ex.: john_doe',
+                'class': 'placeholder-text',
+            }),
+            'email': forms.TextInput(attrs={
+                'placeholder': 'Ex.: johndoe@john.com',
+                'class': 'placeholder-text',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Digite sua senha',
+                'class': 'placeholder-text',
+            }),
+        }

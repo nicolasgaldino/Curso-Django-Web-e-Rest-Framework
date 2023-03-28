2a0812bcb5e2e68377ed0a3f542f03b7484e6433
Nícolas Galdino <nicolasesmael1998@gmail.com>
2023-03-28 09:31:22 -0300

    Envio e salvamento do formulário no banco adicionado.

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,6 @@
 from .forms import RegisterForm
 from django.http import Http404
+from django.contrib import messages
 from django.shortcuts import render
 from django.shortcuts import redirect
 
@@ -19,6 +20,11 @@ def register_create(request):
 
     POST = request.POST
     request.session['register_form_data'] = POST
-    form = RegisterForm(POST)  # noqa F841
+    form = RegisterForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Usuário cadastrado com sucesso.')
+        del(request.session['register_form_data'])  # noqa E275
 
     return redirect('authors:register')

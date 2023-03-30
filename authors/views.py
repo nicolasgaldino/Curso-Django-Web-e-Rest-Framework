d1ed5723b391a5c3ebc714440e2bef7018639aba
Nícolas Galdino <nicolasesmael1998@gmail.com>
2023-03-30 10:20:58 -0300

    Erro de criptografia de senhas corrigido.

--- authors/views.py
+++ authors/views.py
@@ -23,7 +23,9 @@ def register_create(request):
     form = RegisterForm(POST)
 
     if form.is_valid():
-        form.save()
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
         messages.success(request, 'Usuário cadastrado com sucesso.')
         del(request.session['register_form_data'])  # noqa E275
 

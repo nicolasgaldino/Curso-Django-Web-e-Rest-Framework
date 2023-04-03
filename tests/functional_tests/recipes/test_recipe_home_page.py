b20087f92466bab646038796c6919afd641c2eda
Nicolas Galdino <nicolasesmael1998@gmail.com>
2023-04-03 13:24:23 -0300

    Arquivos '__init__' adicionados em diretórios extras para criar o arquivo base de teste funcional.

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,20 +1,5 @@
-import time
+from .base import RecipeBaseFunctionalTest
 from selenium.webdriver.common.by import By
-from utils.browser import make_chrome_browser
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-
-
-class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
-    def setUp(self) -> None:
-        self.browser = make_chrome_browser()
-        return super().setUp()
-
-    def tearDown(self) -> None:
-        self.browser.quit()
-        return super().tearDown()
-
-    def sleep(self, seconds=5):
-        time.sleep(seconds)
 
 
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):

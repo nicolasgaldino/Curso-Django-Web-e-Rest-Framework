7235fcc1e6c3b880c8c8c7adacd1ff16a15728a3
Nicolas Galdino <nicolasesmael1998@gmail.com>
2023-04-03 13:33:18 -0300

    Teste funcionais agrupados com markers.

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,7 +1,9 @@
+import pytest
 from .base import RecipeBaseFunctionalTest
 from selenium.webdriver.common.by import By
 
 
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)

f4b4e91d1a9ae0bb1d3ae08ebfef4e36d5201cbc
Nicolas Galdino <nicolasesmael1998@gmail.com>
2023-04-04 07:46:38 -0300

    Teste funcional de paginação adicionado.

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -30,7 +30,7 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
             '//input[@placeholder="Busque por uma receita..."]'
         )
 
-        # usuário clica no input e busca por uma receita "Título da Receita 1"
+        # usuário clica no input e busca por uma receita
         search_input.send_keys(title_needed)
         search_input.send_keys(Keys.ENTER)
 
@@ -38,3 +38,18 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
             title_needed,
             self.browser.find_element(By.TAG_NAME, 'body').text,
         )
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_home_page_pagination(self):
+        self.make_recipe_in_batch()
+
+        # Usuário abre a página
+        self.browser.get(self.live_server_url)
+
+        # Usuário vê que tem paginação e clica na página 2
+        self.browser.find_element(
+            By.XPATH,
+            '//nav[@aria-label="Pagination"]'
+        )
+
+        self.sleep()
